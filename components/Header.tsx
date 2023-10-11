ee3e6e38552f7cef0495bac9842cbb720e796a01
okatantow <okatantow@gmail.com>
2023-10-11 16:54:43 +0000

    Heder design

---
+++ components/Header.tsx
@@ -0,0 +1,64 @@
+import Image from 'next/image'
+import React from 'react'
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon
+
+} from '@heroicons/react/outline';
+import {HomeIcon} from '@heroicons/react/solid';
+
+function Header() {
+  return (
+    <div>
+        <div className='flex justify-between bg-white max-w-6xl mx-5 xl:mx-auto'>
+        {/* left */}
+         <div className='relative hidden lg:inline-grid w-24'>
+            <Image src='https://links.papareact.com/ocw'
+            layout='fill'
+            objectFit='contain'
+            alt='logo'
+            />
+         </div>
+         <div className='relative w-10  lg:hidden flex-shrink-0 cursor-pointer'>
+            <Image src='https://links.papareact.com/jjm'
+            layout='fill'
+            objectFit='contain'
+            alt='logo'
+            />
+         </div>
+        {/* middle - seach field*/}
+        <div className='max-w-xs'>
+        <div className='relative mt-1 p-3 rounded-md'>
+          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+            <SearchIcon className='h-5 w-5 text-gray-500' />
+          </div>
+          <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gra-300 focus:ring-black focus:border-black rounded-md' type='text' placeholder='Search' />
+        </div>
+        </div>
+
+        {/* right */}
+        <div className='flex items-center justify-end space-x-4'>
+        <HomeIcon className='navBtn' />
+        <MenuIcon className='h-6 md:hidden cursor-pointer ' />
+        <div className='relative navBtn'>
+        <PaperAirplaneIcon className='navBtn rotate-45'/>
+        <div className='absolute -top-1 -right-2 text-xs w-5 h-5
+         bg-red-600 rounded-full flex items-center justify-center
+         animate-pulse text-white'>4</div>
+        </div>
+        <PlusCircleIcon className='navBtn'/>
+        <UserGroupIcon className='navBtn'/>
+        <HeartIcon className='navBtn'/>
+
+        <img className='h-10 rounded-full cursor-pointer' src='https://links.papareact.com/3ke'/>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
